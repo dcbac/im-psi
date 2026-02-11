c93b27f8c38833f4393987cd6146beaf70cae65f
dc138 <antonio@adaro.eu>
2026-02-11 17:59:37 +0100

    feat: añadir datos de sesión a plantilla

--- p1/src/catalog/views.py
+++ p1/src/catalog/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from .models import Book, Author, BookInstance, Genre
 from django.views import generic
 
+
 def index(request):
     """View function for home page of site."""
 
@@ -20,9 +21,9 @@ def index(request):
     num_authors = Author.objects.count()
 
     # Number of visits to this view, as counted in the session variable.
-    num_visits = request.session.get('num_visits', 0)
+    num_visits = request.session.get("num_visits", 0)
     num_visits += 1
-    request.session['num_visits'] = num_visits
+    request.session["num_visits"] = num_visits
 
     context = {
         "num_books": num_books,
@@ -31,26 +32,30 @@ def index(request):
         "num_authors": num_authors,
         "num_genres": num_genres,
         "num_books_containing_a": num_books_containing_a,
+        "num_visits": num_visits,
     }
 
     # Render the HTML template index.html with the data in the context variable
     return render(request, "index.html", context=context)
 
+
 class BookListView(generic.ListView):
     model = Book
     # context_object_name = 'book_list'   # your own name for the list as a template variable
     paginate_by = 2
-    ordering = ['title']
+    ordering = ["title"]
     # queryset = Book.objects.filter(title__icontains='war')[:5] # Get 5 books containing the title war
     # template_name = 'books/my_arbitrary_template_name_list.html'  # Specify your own template name/location
 
+
 class BookDetailView(generic.DetailView):
     model = Book
+
 
 class AuthorListView(generic.ListView):
     model = Author
     paginate_by = 2
 
+
 class AuthorDetailView(generic.DetailView):
     model = Author
-
